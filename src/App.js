7e10a3a14a8880fe5d7df28b89910d42dee52e50
Pedro Emanuel dos Santos Paiva <pedro_lous@hotmail.com>
2022-06-15 18:54:46 -0300

    Crianção do componente card de produtos

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Cardproduto from './components/Cardproduto';
+import styled from 'styled-components';
 
-function App() {
+
+
+
+class App extends React.Component {
+
+  state = {
+    produtos: [
+      {fotoproduto:"https://upload.wikimedia.org/wikipedia/commons/c/c7/Saturn_during_Equinox.jpg",
+       nomeproduto: "Viagem de IDA/VOLTA ao Planeta Saturno.",
+       valorproduto:"50.000"},
+      
+      {
+        fotoproduto: "https://img.olhardigital.com.br/wp-content/uploads/2021/09/Ilustracao-de-Jupiter.jpg",
+        nomeproduto: "Viagem de IDA/VOLTA ao planeta Jupiter!",
+        valorproduto: "70.000"
+      },
+      {fotoproduto: "https://static.mundoeducacao.uol.com.br/mundoeducacao/2021/04/planeta-urano.jpg",
+      nomeproduto: "Viagem de IDA/VOLTA ao planeta Urano!",
+      valorproduto: "90.000"
+    },
+      {
+        fotoproduto: "https://www.infoescola.com/wp-content/uploads/2008/04/planeta-v%C3%AAnus_60584053.jpg",
+        nomeproduto: "Viagem de IDA/VOLTA ao planeta Vênus!",
+       valorproduto: "130.000"
+      }
+
+    ]
+  }
+
+
+  render() {
+
+    const listaDeProdutos = this.state.produtos.map((produto)=> {
+    return (
+     <Cardproduto
+        fotoproduto={produto.fotoproduto }
+        nomeproduto={produto.nomeproduto}
+        valorproduto={produto.valorproduto}
+        />
+    );
+} )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div> 
+          <div>{listaDeProdutos}</div>
+     </div>
+    )
+  }
 }
 
 export default App;
+
+
---
+++ src/components/CardProduto.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const Produto = styled.div`
+  border: 2px solid gray;
+  width: 300px;
+  margin-bottom: 10px;
+  align-items: center;
+  margin: 20px;
+  border-radius: 5px;
+
+`
+const ProdutoFooter = styled.footer`
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+`
+const Botao = styled.button`
+      border-radius: 20px;
+      background-color: blue;
+`
+const Foto = styled.img`
+    width: 100%;
+    height: 200px;
+`
+
+class Cardproduto extends React.Component {
+  render() {
+    return (
+      <div>
+           <Produto>
+            <Foto src={this.props.fotoproduto} alt="foto.produto"></Foto>
+            <ProdutoFooter>
+            <h3>{this.props.nomeproduto}</h3>
+            <h2>R$ {this.props.valorproduto}</h2>
+            <Botao>COMPRAR</Botao>
+            </ProdutoFooter>
+          </Produto> 
+      </div>
+    )
+  }
+}
+
+export default Cardproduto;
+

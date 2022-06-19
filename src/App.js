6bc5c59bcf8bbf79cc4a539827b4fc61fadc3147
AndreEletherio <andremachadoeletherio@gmail.com>
2022-06-19 11:09:38 -0300

    App Pronto Filtro

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
-import Cardproduto from './components/CardProduto';
+
+import CardProduto from './components/CardProduto';
 import styled from 'styled-components';
 import { Filter } from './components/Filter';
 
 
+const SecaoPrincipal = styled.div`
+    display: flex;
+    flex-direction: row;
+    
+`
+const SecaoProdutos = styled.div`
+   display: flex;
+   flex-direction: row;
+   flex-wrap: wrap;
+    
+`
 
 
 class App extends React.Component {
@@ -13,29 +25,29 @@ class App extends React.Component {
       {
         fotoproduto: "https://upload.wikimedia.org/wikipedia/commons/c/c7/Saturn_during_Equinox.jpg",
         nomeproduto: "Viagem de IDA/VOLTA ao Planeta Saturno.",
-        valorproduto: "50.000"
-      },
-
-      {
-        fotoproduto: "https://img.olhardigital.com.br/wp-content/uploads/2021/09/Ilustracao-de-Jupiter.jpg",
-        nomeproduto: "Viagem de IDA/VOLTA ao planeta Jupiter!",
-        valorproduto: "70.000"
-      },
-      {
-        fotoproduto: "https://static.mundoeducacao.uol.com.br/mundoeducacao/2021/04/planeta-urano.jpg",
-        nomeproduto: "Viagem de IDA/VOLTA ao planeta Urano!",
-        valorproduto: "90.000"
+        valorproduto: 50000
       },
       {
         fotoproduto: "https://www.infoescola.com/wp-content/uploads/2008/04/planeta-v%C3%AAnus_60584053.jpg",
         nomeproduto: "Viagem de IDA/VOLTA ao planeta Vênus!",
-        valorproduto: "130.000"
+        valorproduto: 130000     
+      },
+      {
+        fotoproduto: "https://static.mundoeducacao.uol.com.br/mundoeducacao/2021/04/planeta-urano.jpg",
+        nomeproduto: "Viagem de IDA/VOLTA ao planeta Urano!",
+        valorproduto: 90000
+      },
+      {
+        fotoproduto: "https://img.olhardigital.com.br/wp-content/uploads/2021/09/Ilustracao-de-Jupiter.jpg",
+        nomeproduto: "Viagem de IDA/VOLTA ao planeta Jupiter!",
+        valorproduto: 70000
       }
 
     ],
     minValue: -Infinity,
     maxValue: +Infinity,
     search: '',
+    sortBy: '',
   }
 
   getMinValue = (e) => {
@@ -43,34 +55,59 @@ class App extends React.Component {
       minValue: e.target.value
     })
   }
-
   getMaxValue = (e) => {
     this.setState({
       maxValue: e.target.value
     })
   }
-
   onChangeSearchProduct = (e) => {
     this.setState({
       search: e.target.value
     })
   }
-
   onClickCleanFilters = () => {
     this.setState({
       minValue: -Infinity,
       maxValue: +Infinity,
       search: '',
-      order: '',
+      sortBy: '',
+    })
+  }
+  onChangeSelectSort = (e) => {
+    this.setState({
+      sortBy: e.target.value
     })
   }
 
 
-  render() {
 
-    const listaDeProdutos = this.state.produtos.map((produto) => {
+
+  render() {
+    console.log(this.state.sortBy)
+
+
+    const listaDeProdutos = this.state.produtos.filter((product) => {
+      return product.valorproduto >= this.state.minValue || this.state.minValue === "" ? true : false
+    }).filter((product) => {
+      return product.valorproduto <= this.state.maxValue || this.state.maxValue === "" ? true : false
+    }).filter((product) => {
+      return product.nomeproduto.toLowerCase().includes(this.state.search.toLowerCase())
+    }).sort((a, b) => {
+      switch (this.state.sortBy) {
+        case "asc value":
+          return a.valorproduto - b.valorproduto
+        case "desc value":
+          return b.valorproduto - a.valorproduto
+        case "alf a-z":
+          return a.nomeproduto.localeCompare(b.nomeproduto)
+        case "alf z-a":
+          return b.nomeproduto.localeCompare(a.nomeproduto)
+        default:
+          return a, b
+      }
+    }).map((produto) => {
       return (
-        <Cardproduto
+        <CardProduto
           fotoproduto={produto.fotoproduto}
           nomeproduto={produto.nomeproduto}
           valorproduto={produto.valorproduto}
@@ -79,42 +116,27 @@ class App extends React.Component {
     })
 
     return (
-
       <div>
-        <div className="App">
+        <SecaoPrincipal>
+
           <Filter
             getMinValue={this.getMinValue}
             getMaxValue={this.getMaxValue}
             onChangeSearchProduct={this.onChangeSearchProduct}
             onClickCleanFilters={this.onClickCleanFilters}
+            onChangeSelectSort={this.onChangeSelectSort}
             minValue={this.state.minValue}
             maxValue={this.state.maxValue}
             searchProduct={this.state.search}
           />
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-            </a>
-          </header>
-        </div>
-
-=======
-      <div>
-          <div>{listaDeProdutos}</div>
-     </div>
+          <SecaoProdutos>
+            {listaDeProdutos}
+          </SecaoProdutos>
 
 
-   </div>
-)
-}}
+        </SecaoPrincipal>
+      </div>
+    )
+  }
+}
 export default App;
-
-
